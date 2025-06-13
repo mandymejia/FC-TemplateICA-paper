b50a5efae8ecd368f9d86f066e61023d608f3de8
7konigsberg <zeshawn.zahid@gmail.com>
2025-06-13 14:14:54 -0400

    restructured 00_get_TCs_HCP.R

--- simulation/00_get_TCs_HCP.R
+++ simulation/00_get_TCs_HCP.R
@@ -12,36 +12,6 @@ data_dir <- '/N/project/hcp_dcwan' # HCP data directory, set path
 
 cifti_fname <- 'rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas.dtseries.nii' 
 
-#get list of subjects
-numfiles <- table(substr(list.files(data_dir), 1, 6))
-subjects <- names(numfiles)
-N <- length(subjects)
-
-#pick ICs to generate TCs
-GICA_fname <- 'melodic_IC_25.dscalar.nii'
-GICA <- as.matrix(read_cifti(GICA_fname))
-ICs <- c(1, 3, 4, 2, 16) #three visual ICs, 1 DMN, 1 motor
-L <- length(ICs)
-
-#file path to all subjects
-cifti_fullnames <- file.path(data_dir, subjects, cifti_fname)
-
-Amats <- array(dim=c(1200, L, N))
-for(ii in 1:N){
-  
-  if(!file.exists(cifti_fullnames[ii])) next()
-  print(ii)
-  BOLD_ii <- as.matrix(read_cifti(cifti_fullnames[ii]))
-  if(ncol(BOLD_ii) != 1200) next() 
-  DR_ii <- dual_reg(BOLD_ii, GICA, detrend_DCT=10)
-  Amats[,,ii] <- DR_ii$A[,ICs]
-  
-  if(ii/100 == round(ii/100)) {
-    print('saving')
-    saveRDS(Amats, 'simulation/TCs.RDS')
-  }
-}
-
 ###########################################################################################
 ### Hardcoded dual_reg function from an older version of templateICAr package used      ###
 ### exclusively for this script.                                                        ###
@@ -112,3 +82,38 @@ dual_reg <- function(
   #return result
   list(S = S, A = A)
 }
+
+###########################################################################################
+### Begin Script                                                                        ###
+###########################################################################################
+
+#get list of subjects
+numfiles <- table(substr(list.files(data_dir), 1, 6))
+subjects <- names(numfiles)
+N <- length(subjects)
+
+#pick ICs to generate TCs
+GICA_fname <- 'melodic_IC_25.dscalar.nii'
+GICA <- as.matrix(read_cifti(GICA_fname))
+ICs <- c(1, 3, 4, 2, 16) #three visual ICs, 1 DMN, 1 motor
+L <- length(ICs)
+
+#file path to all subjects
+cifti_fullnames <- file.path(data_dir, subjects, cifti_fname)
+
+Amats <- array(dim=c(1200, L, N))
+for(ii in 1:N){
+  
+  if(!file.exists(cifti_fullnames[ii])) next()
+  print(ii)
+  BOLD_ii <- as.matrix(read_cifti(cifti_fullnames[ii]))
+  if(ncol(BOLD_ii) != 1200) next() 
+  DR_ii <- dual_reg(BOLD_ii, GICA, detrend_DCT=10)
+  Amats[,,ii] <- DR_ii$A[,ICs]
+  
+  if(ii/100 == round(ii/100)) {
+    print('saving')
+    saveRDS(Amats, 'simulation/TCs.RDS')
+  }
+}
+
